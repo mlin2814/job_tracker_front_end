481314b2af16bf1eff515beb3a73f2dd6b2d11db
Matthew <mlin2814@gmail.com>
2022-11-15 06:46:08 -0500

    Removed date

--- src/features/auth/Welcome.js
+++ src/features/auth/Welcome.js
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 
 const Welcome = () => {
-    const date = new Date()
-    const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date)
+    // const date = new Date()
+    // const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date)
 
     const content = (
         <section className="welcome">
 
-            <p>{today}</p>
+            {/* <p>{today}</p> */}
 
             <h1>Welcome!</h1>
 
